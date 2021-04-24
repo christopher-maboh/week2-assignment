b238f69e39531d1de175255a8e54de67cf694f2a
Maboh Christopher <christopher.maboh@yahoo.com.com>
2021-04-24 14:43:18 -0400

    week-2-assignment

---
+++ blog-homepage/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import yourArticles from './your-articles.json'
+import Article from './Article'
+import Article1 from './Article1'
+function App() {
+  return (
+    <div className="App">
+   <Article/>
+   <Article1/>
+    </div>
+  );
+}
+
+export default App;
